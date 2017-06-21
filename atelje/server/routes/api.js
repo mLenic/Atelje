110eff98456e1707be9c1a1599eefd4912197c90
Matevz Lenic <matevz.lenic@gmail.com>
2017-06-21 20:11:07 +0200

    Added home component and tested node routes

--- atelje/server/routes/api.js
+++ atelje/server/routes/api.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const axios = require('axios');
+const API = 'https://jsonplaceholder.typicode.com';
+
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works');
+});
+
+router.get('/posts', (req, res) =>{
+    axios.get(`${API}/posts`)
+    .then(posts => {
+      console.log(posts.data);
+      res.status(200).send(posts.data);
+    })
+    .catch(error => {
+      res.status(500).send(error)
+    });
 });
 
 module.exports = router;
