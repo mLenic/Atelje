d4fe30f91254b446e31094845cb036aca950d41e
Matevz Lenic <matevz.lenic@gmail.com>
2017-06-29 20:02:58 +0200

    Added routing for modules

--- atelje/src/app/app-routing.module.ts
+++ atelje/src/app/app-routing.module.ts
@@ -11,6 +11,9 @@ import { ContactComponent } from './contact/contact.component';
 const routes: Routes = [
      { path: '',        redirectTo: '/home',        pathMatch: 'full'   },
      { path: 'home',    component: HomeComponent                        },
+     { path: 'about',   component: AboutComponent                       },
+     { path: 'contact', component: ContactComponent                     },
+     { path: 'blog',    component: BlogComponent                        },
      { path: '**',      component: HomeComponent                        }
 ];
 
