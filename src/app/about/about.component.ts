48ea17af165c6aa72d38967d2f89562d4a46d27b
Matevz Lenic <matevz.lenic@gmail.com>
2018-01-21 22:06:06 +0100

    Some changes - Added mixcloud

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var SC: any;
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,8 +9,8 @@ import { AboutComponent }   from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-     { path: '',        redirectTo: '/home',        pathMatch: 'full'   },
-     { path: 'home',    component: HomeComponent                        },
+     { path: '',        redirectTo: '/domov',        pathMatch: 'full'   },
+     { path: 'domov',    component: HomeComponent                        },
      { path: 'about',   component: AboutComponent                       },
      { path: 'contact', component: ContactComponent                     },
      { path: 'blog',    component: BlogComponent                        },
