e1202b4553ca8aaea0a0a6d60d00ba46ecf4ca0c
Matevz Lenic <matevz.lenic@gmail.com>
2018-03-15 20:47:35 +0100

    Added globals for base url - changes in services

--- src/app/contact/contact.service.ts
+++ src/app/contact/contact.service.ts
@@ -2,14 +2,19 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import { Globals } from '../../../globals';
+
 @Injectable()
 export class ContactService {
 
-    constructor(private http: Http){}
+    constructor(
+        private http: Http,
+        private globals: Globals,
+    ){}
 
     sendContactMail(msgJson){
 
-        var url= process.env.herokuAdd || "http://localhost:3000";
+        var url= this.globals.HTTP_GLOBAL_ADDRESS || "http://localhost:3000";
         url += "/api/mailing/contact/new";
         
         return this.http
--- src/app/hidden/service/hidden.service.ts
+++ src/app/hidden/service/hidden.service.ts
@@ -2,14 +2,19 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import { Globals } from '../../../../globals';
+
 @Injectable()
 export class HiddenService {
 
-    constructor(private http: Http){}
+    constructor(
+        private http: Http,
+        private globals: Globals,
+    ){}
 
     sendContactMail(msgJson){
 
-        var url= process.env.herokuAdd || "http://localhost:3000";
+        var url= this.globals.HTTP_GLOBAL_ADDRESS || "http://localhost:3000";
         url += "/api/mailing/contact/new";
         
         return this.http
@@ -20,7 +25,7 @@ export class HiddenService {
     }
 
     login(msgJson){
-        var url= process.env.herokuAdd || "http://localhost:3000";
+        var url= this.globals.HTTP_GLOBAL_ADDRESS || "http://localhost:3000";
         url += "/api/login";
         console.log("Sending to API");
         return this.http
