f2cdf62e9bd64173809daf81a0792216960cd62a
Matevz Lenic <matevz.lenic@gmail.com>
2018-03-18 13:49:31 +0100

    Change

--- globals.ts
+++ globals.ts
@@ -12,7 +12,7 @@ export class Globals {
     constructor(platformLocation: PlatformLocation){
         //this.HTTP_GLOBAL_ADDRESS = (platformLocation as any).location.hostname; //Try this on production
         //this.HTTP_GLOBAL_ADDRESS = "http://terapevtski-atelje.herokuapp.com";
-        this.HTTP_GLOBAL_ADDRESS = "http://terapevtski-atelje-testing.herokuapp.com/";
+        this.HTTP_GLOBAL_ADDRESS = "http://terapevtski-atelje-testing.herokuapp.com";
         //this.HTTP_GLOBAL_ADDRESS = "http://localhost:3000";
     }
 }
